42a07132c15ba39e73a7e863ce87e0c32ba6bde7
Namita4903 <mahajannamita2003@gmail.com>
2025-05-20 18:41:36 +0530

    a

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
